d48532cea980daa1e0909cb1708b2514ecb54c3b
Watson <ripppony@gmail.com>
2018-06-30 13:50:47 +0100

    Add missing sprite handling

--- src/spritesheet.py
+++ src/spritesheet.py
@@ -4,6 +4,8 @@ import os.path
 import yaml
 
 class SpriteSheet:
+    DEFAULT_SPRITE = {"left":0,"top":0,"width":16,"height":16}
+
     def __init__(self, sheet_filename, config_filename):
         image = pygame.image.load(os.path.join(ASSET_DIRECTORY, sheet_filename)).convert()
         self.sheet = pygame.transform.scale(image, image.get_width()* SCALE_FACTOR, image.get_height()* SCALE_FACTOR)
@@ -12,9 +14,11 @@ class SpriteSheet:
             self.config = yaml.load(config)
 
     def get_sprite(self, name):
-        sprite_config = self.config[name]
+        sprite_config = self.config.get(name,self.DEFAULT_SPRITE)
+
         left = sprite_config["left"] * SCALE_FACTOR
         top = sprite_config["top"] * SCALE_FACTOR
         width = sprite_config["width"] * SCALE_FACTOR
         height = sprite_config["height"] * SCALE_FACTOR
+
         return self.sheet.subsurface((left, top, width, height))
