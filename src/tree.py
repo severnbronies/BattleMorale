84637b654912e845cebd1466116bd651c2470c69
Kit Manners <kitm@nne.rs>
2018-06-30 17:09:15 +0100

    Load level.yaml files in Tree, closes #2

--- src/tree.py
+++ src/tree.py
@@ -1,9 +1,33 @@
+from utils import ordered_load
+
+
 class Tree:
     def __init__(self, filename):
-        pass
+        data = ordered_load(open(filename, "r"))
+        self.nodes = data["nodes"]
+        self.current_node_name = list(self.nodes.keys())[0]
+        self.current_action_index = 0
 
     def pre_update(self, game):
-        pass
+        # obtain current action and arguments
+        current_node = self.nodes[self.current_node_name]
+        current_action = current_node[self.current_action_index]
+        action_func = current_action["type"]
+        kwargs = dict(current_action.items())
+        del kwargs["type"]
 
-    def post_update(self, game, state):
-        pass
+        # call action func with arguments
+        getattr(game, action_func)(**kwargs)
+
+    def post_update(self, state):
+        # obtain current action name
+        current_node = self.nodes[self.current_node_name]
+        current_action = current_node[self.current_action_index]
+        current_action_name = current_action["type"]
+
+        # return choice
+        if current_action_name == "add_pc_choices":
+            self.current_node_name = state
+            self.current_action_index = 0
+        else:
+            self.current_action_index += 1
---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import yaml
+from collections import OrderedDict
+
+
+def ordered_load(stream, Loader=yaml.Loader):
+    class OrderedLoader(Loader):
+        pass
+
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return OrderedDict(loader.construct_pairs(node))
+
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+
+    return yaml.load(stream, OrderedLoader)
