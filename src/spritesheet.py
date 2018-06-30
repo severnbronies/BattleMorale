7018a0bfbff20a83134e426f388d2e5ce03c6ea3
Samuel Partridge <sampartr@gmail.com>
2018-06-30 16:06:43 +0100

    replace convert with convert_alpha

--- src/spritesheet.py
+++ src/spritesheet.py
@@ -8,7 +8,7 @@ class SpriteSheet:
     DEFAULT_SPRITE = {"left":0,"top":0,"width":DEFAULT_SIZE,"height":DEFAULT_SIZE}
 
     def __init__(self, sheet_filename, config_filename):
-        image = pygame.image.load(os.path.join(ASSET_DIRECTORY, sheet_filename)).convert()
+        image = pygame.image.load(os.path.join(ASSET_DIRECTORY, sheet_filename)).convert_alpha()
         self.sheet = pygame.transform.scale(image, (image.get_width()* SCALE_FACTOR, image.get_height()* SCALE_FACTOR))
 
         with open(os.path.join(ASSET_DIRECTORY,config_filename)) as config:
